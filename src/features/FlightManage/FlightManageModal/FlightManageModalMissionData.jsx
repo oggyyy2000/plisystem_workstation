10ffce1fbc023a3d0bff205ae0c0fde5c2a3832b
oggyyy2000 <tuanbeos47@gmail.com>
2023-12-27 16:00:35 +0700

    style: change modal to dialog and adjust css

--- src/features/FlightManage/FlightManageModal/FlightManageModalMissionData.jsx
+++ src/features/FlightManage/FlightManageModal/FlightManageModalMissionData.jsx
@@ -2,17 +2,18 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Fade from "@mui/material/Fade";
-import Button from "@mui/material/Button";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import TextField from "@mui/material/TextField";
-import { Grid } from "@mui/material";
+import {
+  Grid,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Button,
+  ImageList,
+  ImageListItem,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+} from "@mui/material";
 
 import CloseIcon from "@mui/icons-material/Close";
 import CropFreeIcon from "@mui/icons-material/CropFree";
@@ -107,23 +108,8 @@ const ModalMissionData = ({ schedule_id, implementation_date }) => {
     selectedLabels,
     open,
     urlGetData,
-    getImgData
+    getImgData,
   ]);
-
-  // TEST
-
-  // const handleChooseAllBoxChecked = (e) => {
-  //   if(e.target.checked) {
-  //     Object.keys(imgList2).map((vt) => {
-  //       return imgList2[vt].map((info, index) => {
-  //         console.log("not if sent_check:",index, info)
-  //         if(info.sent_check === 0) {
-  //           // return setChecked([info.img_path])
-  //         }
-  //       })
-  //     })
-  //   }
-  // }
 
   // ------------ Render list mission in one day ------------
   const renderListMission = (timeFlyStart) => {
@@ -187,118 +173,114 @@ const ModalMissionData = ({ schedule_id, implementation_date }) => {
     return Object.keys(imgList2).map((vt) => {
       return (
         <>
-          <div>
-            <div className="line-seperate-items"></div>
+          <div className="line-seperate-items"></div>
 
-            <div className="modal-mission-data__img-list-title">{vt}</div>
-            <ImageList
-              sx={{
-                position: "relative",
-                overflowY: "hidden",
-              }}
-              cols={3}
-            >
-              {imgList2[vt]
-                ?.slice(0, nextImg[vt].loaded)
-                ?.map((info, index) => {
-                  return (
-                    <>
-                      <ImageListItem key={index}>
-                        <div className="modal-mission-data__img-list-items-header">
-                          <TextField
-                            label="Tình trạng"
-                            value={info.label.split("_").join("\n")}
-                            multiline
-                            maxRows={1}
-                            style={{ height: "90%", marginTop: "7px" }}
-                            disabled
-                          />
-
-                          <div>
-                            {/* Zoom Dialog */}
-                            <Button
-                              className="modal-mission-data__zoom-btn"
-                              variant="contained"
-                              onClick={() =>
-                                setOpenZoomingImg(
-                                  process.env.REACT_APP_IMG + info.img_path
-                                )
-                              }
-                            >
-                              <CropFreeIcon />
-                            </Button>
-                            <FlightManageZoomingDialog
-                              info={process.env.REACT_APP_IMG + info.img_path}
-                              openZoomingImg={openZoomingImg}
-                              setOpenZoomingImg={setOpenZoomingImg}
-                            />
-                            {/* Edit label Dialog */}
-                            <FlightManageEditLabelDialog
-                              info={info}
-                              setLabelChanged={setLabelChanged}
-                            />
-                          </div>
-                        </div>
-
-                        <label
-                          for={`choose-img-${info.img_path}`}
-                          className={`modal-mission-data__img-list-items-label ${
-                            info.sent_check === 1 ? "hadsubmitted" : ""
-                          } ${
-                            selectedLabels.includes(info.img_path) ||
-                            info.sent_check === 1
-                              ? "choosed"
-                              : ""
-                          }`}
-                          onClick={() => handleLabelClick(info.img_path)}
-                        >
-                          <img
-                            src={process.env.REACT_APP_IMG + info.img_path}
-                            srcSet={process.env.REACT_APP_IMG + info.img_path}
-                            alt={info.img_path}
-                            loading="lazy"
-                            width={"100%"}
-                            height={"100%"}
-                          />
-                        </label>
-
-                        {selectedLabels.includes(info.img_path) ? (
-                          <div className="checkmark-hadchoosed"></div>
-                        ) : (
-                          <></>
-                        )}
-
-                        {info.sent_check === 1 ? (
-                          <div className="checkmark-hadsent"></div>
-                        ) : (
-                          <></>
-                        )}
-                      </ImageListItem>
-
-                      <input
-                        id={`choose-img-${info.img_path}`}
-                        type="checkbox"
-                        value={info.img_path}
-                        style={{
-                          display: "none",
-                        }}
-                        onChange={handleInputClick}
+          <div className="modal-mission-data__img-list-title">{vt}</div>
+          <ImageList
+            sx={{
+              position: "relative",
+              overflowY: "hidden",
+            }}
+            cols={3}
+          >
+            {imgList2[vt]?.slice(0, nextImg[vt].loaded)?.map((info, index) => {
+              return (
+                <>
+                  <ImageListItem key={index}>
+                    <div className="modal-mission-data__img-list-items-header">
+                      <TextField
+                        label="Tình trạng"
+                        value={info.label.split("_").join("\n")}
+                        multiline
+                        maxRows={1}
+                        style={{ height: "90%", marginTop: "7px" }}
+                        disabled
                       />
-                    </>
-                  );
-                })}
-            </ImageList>
 
-            {nextImg[vt].loaded < imgList2[vt]?.length && (
-              <Button
-                className="modal-mission-data__load-more-btn"
-                variant="outlined"
-                onClick={() => handleLoadMore(vt)}
-              >
-                Load more
-              </Button>
-            )}
-          </div>
+                      <div>
+                        {/* Zoom Dialog */}
+                        <Button
+                          className="modal-mission-data__zoom-btn"
+                          variant="contained"
+                          onClick={() =>
+                            setOpenZoomingImg(
+                              process.env.REACT_APP_IMG + info.img_path
+                            )
+                          }
+                        >
+                          <CropFreeIcon />
+                        </Button>
+                        <FlightManageZoomingDialog
+                          info={process.env.REACT_APP_IMG + info.img_path}
+                          openZoomingImg={openZoomingImg}
+                          setOpenZoomingImg={setOpenZoomingImg}
+                        />
+                        {/* Edit label Dialog */}
+                        <FlightManageEditLabelDialog
+                          info={info}
+                          setLabelChanged={setLabelChanged}
+                        />
+                      </div>
+                    </div>
+
+                    <label
+                      for={`choose-img-${info.img_path}`}
+                      className={`modal-mission-data__img-list-items-label ${
+                        info.sent_check === 1 ? "hadsubmitted" : ""
+                      } ${
+                        selectedLabels.includes(info.img_path) ||
+                        info.sent_check === 1
+                          ? "choosed"
+                          : ""
+                      }`}
+                      onClick={() => handleLabelClick(info.img_path)}
+                    >
+                      <img
+                        src={process.env.REACT_APP_IMG + info.img_path}
+                        srcSet={process.env.REACT_APP_IMG + info.img_path}
+                        alt={info.img_path}
+                        loading="lazy"
+                        width={"100%"}
+                        height={"100%"}
+                      />
+                    </label>
+
+                    {selectedLabels.includes(info.img_path) ? (
+                      <div className="checkmark-hadchoosed"></div>
+                    ) : (
+                      <></>
+                    )}
+
+                    {info.sent_check === 1 ? (
+                      <div className="checkmark-hadsent"></div>
+                    ) : (
+                      <></>
+                    )}
+                  </ImageListItem>
+
+                  <input
+                    id={`choose-img-${info.img_path}`}
+                    type="checkbox"
+                    value={info.img_path}
+                    style={{
+                      display: "none",
+                    }}
+                    onChange={handleInputClick}
+                  />
+                </>
+              );
+            })}
+          </ImageList>
+
+          {nextImg[vt].loaded < imgList2[vt]?.length && (
+            <Button
+              className="modal-mission-data__load-more-btn"
+              variant="outlined"
+              onClick={() => handleLoadMore(vt)}
+            >
+              Load more
+            </Button>
+          )}
         </>
       );
     });
@@ -333,81 +315,48 @@ const ModalMissionData = ({ schedule_id, implementation_date }) => {
         xem dữ liệu
       </Button>
 
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={() => setOpen(false)}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            timeout: 500,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box
-            className="modal-mission-data"
-            sx={{
-              bgcolor: "background.paper",
-            }}
-          >
-            <Grid
-              container
-              className="modal-mission-data__container"
-              spacing={0}
+      <Dialog fullScreen open={open} onClose={() => setOpen(false)}>
+        <DialogTitle className="modal-mission-data__header">
+          <FormControlLabel
+            className="modal-mission-data__form-label"
+            control={<Checkbox />}
+            label="Ảnh bất thường"
+            onChange={(e) => handleErrorImageBoxChecked(e)}
+          />
+          <div className="modal-mission-data__btn-group">
+            <FlightManagePrintReportDialog
+              implementation_date={implementation_date}
+            />
+
+            <Button
+              className="modal-mission-data__submit-btn"
+              variant="outlined"
+              onClick={handleSubmit}
             >
-              <Grid item className="modal-mission-data__list-mission" xs={2}>
-                {renderListMission(timeFlyStart)}
-              </Grid>
-              <Grid container xs={10}>
-                <Grid item className="modal-mission-data__header" xs={12}>
-                  <FormControlLabel
-                    className="modal-mission-data__form-label"
-                    control={<Checkbox />}
-                    label="Ảnh bất thường"
-                    onChange={(e) => handleErrorImageBoxChecked(e)}
-                  />
-                  {/* <FormControlLabel
-                    control={<Checkbox />}
-                    label="Chọn tất cả"
-                    style={{ margin: 0, height: "33px" }}
-                    onChange={(e) => handleChooseAllBoxChecked(e)}
-                  /> */}
-                  <div className="modal-mission-data__btn-group">
-                    <FlightManagePrintReportDialog
-                      implementation_date={implementation_date}
-                    />
+              SUBMIT
+            </Button>
 
-                    <Button
-                      className="modal-mission-data__submit-btn"
-                      variant="outlined"
-                      onClick={handleSubmit}
-                    >
-                      SUBMIT
-                    </Button>
-
-                    <Button
-                      className="modal-mission-data__close-btn"
-                      color="error"
-                      variant="contained"
-                      onClick={() => setOpen(false)}
-                    >
-                      <CloseIcon fontSize="small" />
-                    </Button>
-                  </div>
-                </Grid>
-                <Grid item className="modal-mission-data__body" xs={12}>
-                  <div className="modal-mission-data__img-list">
-                    {renderImageList()}
-                  </div>
-                </Grid>
-              </Grid>
+            <Button
+              className="modal-mission-data__close-btn"
+              color="error"
+              variant="contained"
+              onClick={() => setOpen(false)}
+            >
+              <CloseIcon fontSize="small" />
+            </Button>
+          </div>
+        </DialogTitle>
+        <DialogContent className="modal-mission-data__body">
+          <Grid container spacing={0} className="modal-mission-data__body">
+            <Grid item className="modal-mission-data__list-mission" xs={2}>
+              {renderListMission(timeFlyStart)}
             </Grid>
-          </Box>
-        </Fade>
-      </Modal>
+            <Grid item xs={10} className="modal-mission-data__img-list">
+              {renderImageList()}
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
